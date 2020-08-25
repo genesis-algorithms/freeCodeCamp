25a2cde9632b694693af1d4ebb734dbfefa5b4b7
genesis-algorithms <genesisalgorithms@gmail.com>
2020-08-25 23:51:09 +0300

    JavaScript Algorithms and Data Structures: 14/23 of Functional Programming is complete!

--- 02-javascript-algorithms-and-data-structures/08-functional-programming/index.js
+++ 02-javascript-algorithms-and-data-structures/08-functional-programming/index.js
@@ -659,3 +659,21 @@ const printTitle = title => {
   /* concatenatedArray: (5) [1, 2, 3, 4, 5] */
   console.groupEnd();
 })();
+
+(() => {
+  printTitle('14 - Add Elements to the End of an Array Using concat Instead of push');
+  /*
+    Change the nonMutatingPush function so it uses concat to add newItem to the end of original instead of push. The function should return an array.
+  */
+  const nonMutatingPush = (original, newItem) => original.concat(newItem);
+
+  const first = [1, 2, 3];
+  const second = [4, 5];
+
+  const nonMutatedArray = nonMutatingPush(first, second);
+
+  console.log('nonMutatedArray:', nonMutatedArray);
+  /*nonMutatedArray: (5) [1, 2, 3, 4, 5]*/
+  console.groupEnd();
+})();
+
